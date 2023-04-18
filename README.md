78d7c552e3a0e92d6124ee8a76e2d90041e8e5a6
Mikael Eriksson <m.eriksson@gmx.com>
2023-04-18 10:51:59 +0200

    Grunder som dependencies och port 9090, application.properties etc.

--- README.md
+++
@@ -1,5 +0,0 @@
-# BravoCarRental
-
-Car Rental software for WiggelKoncern. 
-
-[Jira Project](https://mikaelmirke.atlassian.net/jira/software/projects/YB/boards/3)
---
+++ src/test/java/com/bravo/gruppbravowigellkoncernencarrental/GruppBravoWigellkoncernenCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravo.gruppbravowigellkoncernencarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppBravoWigellkoncernenCarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
