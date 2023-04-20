2eea37bb128b16a02c8df7a5592afb587ddabb7c
Mikael Eriksson <m.eriksson@gmx.com>
2023-04-20 15:01:24 +0200

    TODO Skeleton for what the AdminController class might need.

---
+++ src/main/java/com/bravo/gruppbravowigellkoncernencarrental/controllers/AdminController.java
@@ -0,0 +1,43 @@
+package com.bravo.gruppbravowigellkoncernencarrental.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <code>AdminController</code> - CRUD commands for admin
+ * @authors Mikael Eriksson (mikael.eriksson@edu.edugrade.se)
+ * @version 0.0.1
+ */
+
+@RestController
+public class AdminController {
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //   Properties
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // TODO - Needs Property Admin Services
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //   Methods CRUD
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // TODO - Needs @GetMapping, for "api/v1/allcars", which uses admin Service to get all cars in database
+
+    // TODO - Needs @PostMapping, for "api/v1/addcar", which uses admin Service to add car to database
+
+    // TODO - Needs @DeleteMapping, for "api/v1/deletecar", which uses admin Service to delete a car in database
+
+    // TODO - Needs @PutMapping, for "api/v1/updatecar", which uses admin Service to update a car to database
+
+    // TODO - Needs @GetMapping, for "api/v1/customers", which uses admin Service get all customers in database
+
+    // TODO - Needs @PostMapping, for "api/v1/addcustomer", which uses admin Service to add a customer to database
+
+    // TODO - Needs @PutMapping, for "api/v1/updatecustomer", which uses admin Service to update a customer in database
+
+    // TODO - Needs @DeleteMapping, for "api/v1/deletecustomer", which uses admin Service to delete a customer in database
+
+    // TODO - Needs @GetMapping, for "api/v1/orders", which uses admin Service to get all orders in database
+
+    // TODO - Needs @DeleteMapping, for "api/v1/deleteorder", which uses admin Service to remove a order in database
+}
