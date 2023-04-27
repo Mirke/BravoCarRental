ec52f7980479142c3689ac792b8b1c23f58f72f4
Mikael Eriksson <m.eriksson@gmx.com>
2023-04-27 12:32:48 +0200

    Added TODOs for OrdersService.java

--- src/main/java/com/bravo/gruppbravowigellkoncernencarrental/services/OrdersService.java
+++ src/main/java/com/bravo/gruppbravowigellkoncernencarrental/services/OrdersService.java
@@ -1,6 +1,8 @@
 package com.bravo.gruppbravowigellkoncernencarrental.services;
 
 import com.bravo.gruppbravowigellkoncernencarrental.entities.Orders;
+import com.bravo.gruppbravowigellkoncernencarrental.repositories.IOrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,26 +16,38 @@ import java.util.List;
 @Service
 public class OrdersService implements IOrdersService {
 
+    @Autowired
+    IOrdersRepository iOrdersRepository;
+
+    // TODO - Needs to get all of the orders in the repository
+
     @Override
     public List<Orders> getAllOrders() {
         return null;
     }
+
+    // TODO - Needs to get all of the orders made by a customer
 
     @Override
     public List<Orders> getCustomerOrders(Long id) {
         return null;
     }
 
+    // TODO - Method for a Customer when they order a car that is available
+
     @Override
     public void orderCar(Long id) {
 
     }
+
+    // TODO - Method for when a Customer decides to cancel and order
 
     @Override
     public void cancelOrder(Long id) {
 
     }
 
+    // TODO - Method for when an Admin decides to remove an order from repository
     @Override
     public void removeOrder(Long id) {
 
